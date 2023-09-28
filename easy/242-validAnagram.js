beb185a69d184b48cd87a726b77c187176d0078c
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2023-09-28 09:24:58 +0300

    Solved 242. Valid Angram

--- easy/242-validAnagram.js
+++ easy/242-validAnagram.js
@@ -15,17 +15,11 @@ const isAnagram = (s, t) => {
   for (let i = 0; i < sLowerCase.length; i++) {
     commonCharfound = false;
     for (let j = 0; j < tLowerCase.length; j++) {
-      console.log(i);
-      console.log("sLowerCase: " + sLowerCase[i])
-      console.log("tLowerCase: " + tLowerCase[j])
-      console.log(sLowerCase[i] == tLowerCase[j]);
       if (sLowerCase[i] == tLowerCase[j]) {
-        console.log(sLowerCase);
-        console.log(tLowerCase);
         commonCharfound = true;
         sLowerCase = sLowerCase.slice(0, i) + sLowerCase.slice(i + 1);
         tLowerCase = tLowerCase.slice(0, j) + tLowerCase.slice(j + 1);
-        i = 0;
+        i = -1;
         break;
       }
     }
@@ -35,5 +29,3 @@ const isAnagram = (s, t) => {
   }
   return true;
 };
-
-console.log(isAnagram("aacc", "ccac"));
